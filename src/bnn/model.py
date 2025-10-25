88c0b3c8522ac89c346be60dec49695ba2439ff8
Diego Quezada <heydiegoq@gmail.com>
2025-10-24 21:51:39 -0300

    add more parameters to bnn

--- src/bnn/model.py
+++ src/bnn/model.py
@@ -2,40 +2,30 @@ import torch
 import torch.nn as nn
 
 class BNN(nn.Module):
-    def __init__(self, input_dim, p=0.1):
+    def __init__(self, input_dim, hidden_layers=[1024, 1024, 1024, 1024], p=0.1):
+        # 1024, 1024, 1024, 1024
         super(BNN, self).__init__()
-        
-        self.fc1 = nn.Linear(input_dim, 400)
-        self.bn1 = nn.BatchNorm1d(400)
-        self.fc2 = nn.Linear(400, 400)
-        self.bn2 = nn.BatchNorm1d(400)
-        self.fc3 = nn.Linear(400, 400)
-        self.bn3 = nn.BatchNorm1d(400)
-        self.fc4 = nn.Linear(400, 400)
-        self.bn4 = nn.BatchNorm1d(400)
-        self.fc5 = nn.Linear(400, 400)
-        self.bn5 = nn.BatchNorm1d(400)
-        self.fc_out = nn.Linear(400, 1)
-        self.dropout = nn.Dropout(p=p)
-        
+
+        layers = []
+        in_dim = input_dim
+
+        for h in hidden_layers:
+            layers.append(nn.Linear(in_dim, h))
+            layers.append(nn.BatchNorm1d(h))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(p))
+            in_dim = h
+
+        layers.append(nn.Linear(in_dim, 1))  # output layer
+
+        self.net = nn.Sequential(*layers)
         self.apply(self._init_weights)
-    
+
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
             nn.init.xavier_uniform_(m.weight)
             if m.bias is not None:
                 nn.init.zeros_(m.bias)
-    
+
     def forward(self, x):
-        x = torch.relu(self.bn1(self.fc1(x)))
-        x = self.dropout(x)
-        x = torch.relu(self.bn2(self.fc2(x)))
-        x = self.dropout(x)
-        x = torch.relu(self.bn3(self.fc3(x)))
-        x = self.dropout(x)
-        x = torch.relu(self.bn4(self.fc4(x)))
-        x = self.dropout(x)
-        x = torch.relu(self.bn5(self.fc5(x)))
-        x = self.dropout(x)
-        x = self.fc_out(x)
-        return x
+        return self.net(x)
